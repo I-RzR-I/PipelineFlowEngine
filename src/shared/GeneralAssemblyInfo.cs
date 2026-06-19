a3224e0cde9893f86cc63b930fcbbb9eadba15be
RzR <108324929+I-RzR-I@users.noreply.github.com>
2026-06-19 23:40:59 +0300

    Update the reference year

--- src/shared/GeneralAssemblyInfo.cs
+++ src/shared/GeneralAssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Resources;
 
 [assembly: AssemblyCompany("RzR SOFT & TECH ®")]
 [assembly: AssemblyProduct("PipelineFlowEngine")]
-[assembly: AssemblyCopyright("Copyright © 2022-2025 RzR All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2022-2026 RzR All rights reserved.")]
 [assembly: AssemblyTrademark("RzR SOFT Solution™")]
 [assembly: AssemblyDescription("The repository implements a pipeline composed of multiple interdependent methods. The execution flows in a single, ordered direction—step by step—with support for enabling or disabling individual steps, customizing execution order, performing pre-execution validation, applying retry policies, collecting execution logs for each step, and more.")]
 
